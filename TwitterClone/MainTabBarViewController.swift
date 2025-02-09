3f8485e92a51371bf05643034ae2cbef30a33084
Aizat <aizatozn@gmail.com>
2025-02-09 12:56:11 -0500

    Reconnecting project to GitHub

---
+++ TwitterClone/MainTabBarViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TwitterClone
+//
+//  Created by Aizat Ozbekova on 9/2/25.
+//
+
+import UIKit
+
+class MainTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBlue
+    }
+
+
+}
+
