526c11fc33c1a3d46e5917bf79be261accef2dc2
aizatozn <aizatozn@gmail.com>
2025-02-11 12:35:26 -0500

    HomeViewController, table view

--- TwitterClone/Controllers/Core/MainTabBarViewController.swift
+++ TwitterClone/Controllers/Core/MainTabBarViewController.swift
@@ -11,7 +11,7 @@ class MainTabBarViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBackground
+        view.backgroundColor = .systemGreen
         
         let vc1 = UINavigationController(rootViewController: HomeViewController())
         let vc2 = UINavigationController(rootViewController: SearchViewController())
