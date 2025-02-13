d65f8ca2f56bc7386457278f920b261738db9ddd
aizatozn <aizatozn@gmail.com>
2025-02-13 12:19:57 -0500

    TweetTableViewCell

--- TwitterClone/Views/TweetTableViewCell.swift
+++ TwitterClone/Views/TweetTableViewCell.swift
@@ -9,15 +9,55 @@ import UIKit
 
 class TweetTableViewCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    static let identifier = "TweetTableViewCell"
+    
+    private let avatarImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFill
+        imageView.layer.cornerRadius = 25
+        imageView.layer.masksToBounds = true
+        imageView.clipsToBounds = true
+        imageView.image = UIImage(systemName: "person")
+        imageView.backgroundColor = .blue
+        return imageView
+    }()
+    
+    private let displayNameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Aizat Ozbekova"
+        label.font = .systemFont(ofSize: 18, weight: .bold)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let userNameLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 16, weight: .regular)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.addSubview(avatarImageView)
+        
+        configureConstraints()
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    private func configureConstraints() {
+        let avatarImageViewConstraints = [
+            avatarImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
+            avatarImageView.topAnchor.constraint(equalTo: topAnchor, constant: 14),
+            avatarImageView.heightAnchor.constraint(equalToConstant: 50),
+            avatarImageView.widthAnchor.constraint(equalToConstant: 50)
+        ]
+        
+        NSLayoutConstraint.activate(avatarImageViewConstraints)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
     }
 
 }
