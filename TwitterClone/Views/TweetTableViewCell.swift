dc53269a327b4dc0617be11eee269b97d2989f3a
aizatozn <aizatozn@gmail.com>
2025-02-13 12:19:50 -0500

    TweetTableViewCell

---
+++ TwitterClone/Views/TweetTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TweetTableViewCell.swift
+//  TwitterClone
+//
+//  Created by Aizat Ozbekova on 13/2/25.
+//
+
+import UIKit
+
+class TweetTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
